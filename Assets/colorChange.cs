5812a70ed10bf69d8a56a149ea7c099ddfd03e9e
1080TI\akira <akirasato@psychic-vr-lab.com>
2018-10-09 23:15:31 +0900

    キーボードでマテリアルの色を変化させるスクリプト

---
+++ Assets/colorChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0.5f, 0.3f, 0.07f);
+		Renderer renderer = GetComponent<Renderer>();
+		Color c = renderer.material.color;
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			c.r += 0.001f;
+			if (c.r > 1.0f) {
+				c.r = 1.0f;
+			}
+			c.b -= 0.001f;
+			if (c.b < 0.0f) {
+				c.b = 0.0f;
+			}
+			renderer.material.color = c;
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			c.r -= 0.001f;
+			if (c.r < 0.0f) {
+				c.r = 0.0f;
+			}
+			c.b += 0.001f;
+			if (c.b > 1.0f) {
+				c.b = 1.0f;
+			}
+			renderer.material.color = c;			
+		}
+	}
+}
---
+++ Assets/textureRandomScale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textureRandomScale : MonoBehaviour {
+	private Renderer renderer;	
+	private float x;
+	private float y;
+	
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Random.Range(1, 5);
+		y = Random.Range(1, 5);
+		renderer.material.mainTextureScale = new Vector2(x, y);
+	}
+}
