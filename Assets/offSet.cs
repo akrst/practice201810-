4d6163735f9e8985f53499b71bd316ea6bddb839
akirasato <>
2018-10-13 21:51:26 +0900

    インスペクターから操作できるオフセット用スクリプト作成 ターゲットオブジェクト、オフセットオブジェクト、Y距離、Z距離を指定可能 エディタ拡張で、セットした時点でインスペクター呼び出し可能

---
+++ Assets/offSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offSet : MonoBehaviour {
+	public GameObject targetObject;
+	public GameObject OffsetObject;
+	public float offsetY;
+	public float offsetZ;
+	
+	void Update () {
+		TrackingPos();
+	}
+	
+	[ContextMenu("TrackingPos")]
+	void TrackingPos() {
+		Vector3 pos = targetObject.transform.position;
+		pos.y += offsetY;
+		pos.z -= offsetZ;
+		OffsetObject.transform.position = pos;
+	}
+}
