dbf06fd28bed72a2ec9e8e4cdb716df4d910cc87
1080TI\akira <akirasato@psychic-vr-lab.com>
2018-10-15 13:38:56 +0900

    スタンダードシェーダーでレンダリングモードを変更するためのスクリプトを作成

---
+++ Assets/chanegStandardShaderRenderingMode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class chanegStandardShaderRenderingMode : MonoBehaviour {
+	private int counter = 0;
+	private GameObject obj = null;
+	
+	
+	// Use this for initialization
+	void Start () {
+		GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
+		foreach (GameObject obj in objs) {
+			Renderer render = obj.GetComponent<Renderer>();
+			render.material.SetFloat("_Mode", 3f);
+			render.material.SetInt("_SrcBlend", (int)BlendMode.SrcAlpha);
+			render.material.SetInt("_DstBlend", (int)BlendMode.OneMinusSrcAlpha);
+			render.material.SetInt("_ZWrite", 0);
+			render.material.DisableKeyword("_ALPHATEST_ON");
+			render.material.EnableKeyword("_ALPHABLEND_ON");
+			render.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+			render.material.renderQueue = 3000;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+			Color color = renderer.material.color;
+			color.a = 0.25f;
+			renderer.material.color = color;
+		}
+	}
+
+	private void OnTriggerExit(Collider collision) {
+		if (collision.gameObject.tag == "Player") {
+			Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+			Color color = renderer.material.color;
+			color.a = 1.0f;
+			renderer.material.color = color;
+		}
+	}
+}
